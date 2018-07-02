86ddfb0a71bc4afd05bc4dfea280894f7e6531e2
nahum sonck <33633504+nahumcoding@users.noreply.github.com>
2018-07-02 19:25:09 -0400

    worked on app folder and components app.js and main.css git commit -m

---
+++ tow-away/app/components/home.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var axios = require('axios');
+
+// var InputBox = require('./InputBox');
+// var PartialWordDisplay = require('./PartialWordDisplay');
+// var GuessedLetters = require('./GuessedLetters');
+// var HangpersonDisplay = require('./HangpersonDisplay');
+
+var Home = React.createClass({
+	// getInitialState: function() {
+	// 	return {
+	// 		game: {
+	// 			partialWord: [],
+	// 			guessed: []
+	// 		}
+	// 	}
+	// },
+	componentWillMount: function() {
+		var self = this;
+	},
+	render: function() {
+		return (
+				 <p>hi</p>
+		);
+	}
+});
+
+module.exports = Home;
